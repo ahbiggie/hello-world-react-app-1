0a05e85d3382241805d1d0d667a297edd78870e4
shaibu lawal yusuf <ahbiggielawal@yahoo.com>
2025-04-01 14:56:56 +0100

    Add a list of items to the App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <ul>
+        <li>First item</li>
+        <li>Second item</li>
+        <li>Third item</li>
+      </ul>
     </div>
   );
 }
